021dc37857ed25afe18306badf07ab89e62d925a
hongli li <anotherdayinpara@yahoo.com>
2010-02-26 23:57:54 -0500

    added new files

---
+++ grails-app/controllers/com/i/SearchController.groovy
@@ -0,0 +1,47 @@
+package com.i
+
+import org.compass.core.engine.SearchEngineQueryParseException
+
+class SearchController
+{
+	def searchableService
+
+    def index =
+	{
+        try
+		{
+			def postInstances = Post.search(params.q, params)
+            if(postInstances.results.size() != 0)
+            {
+              return [postInstances: postInstances.results]
+            }
+            else
+            {
+              return [:];
+            }
+        }
+		catch (SearchEngineQueryParseException ex)
+		{
+            return [parseException: true]
+        }
+    }
+
+	def getSearchSuggestion =
+	{
+		def result
+        if (!params.q?.trim())
+		{
+            result = ''
+        }
+        try
+		{
+			def postInstances = Post.search(params.q, [suggestQuery:true])
+            result = !postInstances.suggestedQuery?'':postInstances.suggestedQuery
+        }
+		catch (SearchEngineQueryParseException ex)
+		{
+            result = ''
+        }
+		render result
+    }
+}
---
+++ grails-app/views/search/index.gsp
@@ -0,0 +1,15 @@
+<%@ page import="org.springframework.util.ClassUtils" %>
+<%@ page import="org.codehaus.groovy.grails.plugins.searchable.SearchableUtils" %>
+<%@ page import="org.codehaus.groovy.grails.plugins.searchable.lucene.LuceneUtils" %>
+<%@ page import="org.codehaus.groovy.grails.plugins.searchable.util.StringQueryUtils" %>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="layout" content="main" />
+  <title>Post List</title>
+</head>
+<body>
+	<g:applyLayout name="../common/emptybody" template="../main/post_items"/>
+	<g:renderRightSideBar/>
+</body>
+</html>
