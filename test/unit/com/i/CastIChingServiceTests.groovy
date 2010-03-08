3931be9c0a898423e370282b58ff2878ae986923
hongli li <anotherdayinpara@yahoo.com>
2010-03-07 22:44:11 -0500

    added gdsflex plugin

---
+++ test/unit/com/i/CastIChingServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.i
+
+import grails.test.*
+
+class CastIChingServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
